dc671ea716d2e453b69ef40c229405b97e8fe967
ovrmrw <t.ngc.0403@gmail.com>
2017-01-01 13:18:36 +0900

    Fix

--- src/lib/simple-store/helper.ts
+++ src/lib/simple-store/helper.ts
@@ -1,6 +1,9 @@
 import { latestUpdatedKey } from './simple-store';
 
 
+/**
+ * Deprecated. Use filterByUpdatedKey operator of RxJS.
+ */
 export function isUpdatedKey<T>(this: string[], state: T): boolean {
   if (!Array.isArray(this)) {
     throw new Error('"this" object of "updatedProperty" function should be an array!');
--- src/lib/simple-store/index.ts
+++ src/lib/simple-store/index.ts
@@ -1,4 +1,4 @@
 export * from './simple-store.module'
 export { SimpleStore } from './simple-store'
 export { QueueConcurrent, InitialState } from './common'
-export { isUpdatedKey } from './helper'
+// export { isUpdatedKey } from './helper'
