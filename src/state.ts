4b39cbffc10c0ddb4e42401f1229add0a210c0a7
Tomohiro Noguchi (@ovrmrw) <t.ngc.0403@gmail.com>
2017-01-06 14:09:19 +0900

    Fix

--- src/state.ts
+++ src/state.ts
@@ -9,7 +9,10 @@ export const initialState: AppState = {
 }
 
 
-/* AppStateの型定義。 */
+/* 
+  AppStateの型定義。 
+  Keyを変更するときは KEY: ObjectKeys<AppState> の中身を変更してからこっちを変更すること。(for VSCode 1.8.1)
+*/
 export interface AppState {
   increment: IncrementState,
   lastUpdated: number,
